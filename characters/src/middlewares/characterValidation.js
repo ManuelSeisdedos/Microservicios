a8cdb4eb7ef18c4346f90ec33593b9db84afd234
ManuelSeisdedos <seisdedosmanuel2@gmail.com>
2023-07-13 11:13:51 -0300

    Validator

--- characters/src/middlewares/characterValidation.js
+++ characters/src/middlewares/characterValidation.js
@@ -2,7 +2,7 @@ import ClientError from "../utils/errors/index.js"
 
 export default (req,res,next,validator) => {
     const name = req.body.name
-    console.log(name)
+    
     if (name) {
         validator.name = name
         return validator
--- characters/src/middlewares/homeworldValidation.js
+++ characters/src/middlewares/homeworldValidation.js
@@ -2,13 +2,8 @@ import ClientError from "../utils/errors/index.js"
 import axios from 'axios'
 export default async (req,res,next,validator) => {
     const {homeworld} = req.body
+    typeof homeworld === "string"? validator.homeworld = null : validator.homeworld === homeworld
     const value = await axios.get(`http://localhost:8004/Planets/${homeworld}`)
-    
-    if (value.data._id) {
-        validator.homeworld = homeworld
-        return validator
-    } else { 
-       validator.homeworld = null
-       return validator
-    }
+    value.data._id? validator.homeworld = homeworld : validator.homeworld = null
+    return validator
 }
