93af59d6810c5d3c1a302756325c01e52d1d0314
ManuelSeisdedos <seisdedosmanuel2@gmail.com>
2023-07-13 11:28:47 -0300

    Films Validator

--- characters/src/middlewares/filmsValidator.js
+++ characters/src/middlewares/filmsValidator.js
@@ -1,11 +1,12 @@
 import ClientError from "../utils/errors/index.js"
 
-export default async (req,res,next) => {
+export default async (req,res,next,validator) => {
     const {films} = req.body
     const value = await axios.get(`http://localhost:8004/Films/${films}`)
     // Desarrollarlo, porque Films va a ser un array con strings de cada pelicula. 
     if (value.data._id) {
-        return next()
+        validator.films = films
+        return validator
     } else { 
        throw new ClientError("Error en el planeta.", 401)
     }
