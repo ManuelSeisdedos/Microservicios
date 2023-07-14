37fe9e22c5a48378ac9132f0a34e63e378f7135a
ManuelSeisdedos <seisdedosmanuel2@gmail.com>
2023-07-14 15:16:48 -0300

    Validator Films.

--- characters/src/middlewares/filmsValidator.js
+++ characters/src/middlewares/filmsValidator.js
@@ -1,8 +1,13 @@
 import ClientError from "../utils/errors/index.js"
+import axios from 'axios'
 
 export default async (req,res,next,validator) => {
     const {films} = req.body
-    const value = await axios.get(`http://localhost:8004/Films/${films}`)
+    let promesas = []
+    for (let index = 0; index < films.length; index++) {
+    promesas.push(axios.get(`http://localhost:8004/Films/${index}`))
+    }
+    const value = await Promise.all(promesas)
     // Desarrollarlo, porque Films va a ser un array con strings de cada pelicula. 
     if (value.data._id) {
         validator.films = films
