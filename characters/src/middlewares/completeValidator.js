7306e2c90ed4397ac7faef6f9902bb752c274a1f
ManuelSeisdedos <seisdedosmanuel2@gmail.com>
2023-07-17 22:47:23 -0300

    Prueba validator

--- characters/src/middlewares/completeValidator.js
+++ characters/src/middlewares/completeValidator.js
@@ -3,8 +3,8 @@ import characterValitador from './characterValidation.js'
 import homeworldValidator from './homeworldValidation.js'
 export default async (req,res,next) => {
     let validator = {}
-    validator = characterValitador(req,res,validator)
-    validator = homeworldValidator(req,res,validator)
-    validator = filmsValidator(req,res, validator)
-    return validator
+    validator = await characterValitador(req,res,validator)
+    validator = await homeworldValidator(req,res,validator)
+    validator = await filmsValidator(req,res, validator)
+    next()
 }
