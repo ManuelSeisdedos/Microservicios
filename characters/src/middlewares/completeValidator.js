e050a639f8c42c5738c3263de721ad143c023993
ManuelSeisdedos <seisdedosmanuel2@gmail.com>
2023-07-21 20:22:30 -0300

    Complete validator

--- characters/src/middlewares/completeValidator.js
+++ characters/src/middlewares/completeValidator.js
@@ -1,11 +1,20 @@
 import filmsValidator from './filmsValidator.js'
 import characterValitador from './characterValidation.js'
 import homeworldValidator from './homeworldValidation.js'
+import ClientError from '../utils/errors/index.js'
 export default async (req,res,next) => {
     let validator = {}
     validator.name = await characterValitador(req,res, validator)
     validator.homeworld = await homeworldValidator(req,res)
-    console.log(validator)
-    validator = await filmsValidator(req,res)
-    next()
+    validator.films = await filmsValidator(req,res)
+    if (
+        validator.name !== null &&
+        validator.homeworld !== null &&
+        validator.films !== null
+    ) {
+        next()
+    } else {
+        
+        throw new ClientError ("Error en la creacion del personaje", 401 )
+    }
 }
